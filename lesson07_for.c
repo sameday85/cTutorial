db128dd1916c946e60027f5c41af40c56a284e03
sameday85 <42813927+sameday85@users.noreply.github.com>
2019-11-10 18:16:48 -0500

    Lession 7

---
+++ lesson07_for.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+int main(){
+  int num = 10;
+  while (num <=20){
+     printf("Hello While\n");
+     num++;
+  }
+  for(int i = 0; i<10; i++) {
+     printf("Hello For\n");
+  }
+}
